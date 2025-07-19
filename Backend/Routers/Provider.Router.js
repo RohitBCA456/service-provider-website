aa21546729d19cf99c45710be4ceeec09a3e6f01
RohitBCA456 <ry2281104@gmail.com>
2025-07-19 21:15:27 +0530

    fix

--- Backend/Routers/Provider.Router.js
+++ Backend/Routers/Provider.Router.js
@@ -1,13 +1,19 @@
 import Router from "express";
-import { registerProvider, updateProviderProfile, getSingleProvider, getALLNearByProviders, logoutProvider } from "../Controllers/Provider.Controller.js";
+import {
+  registerProvider,
+  updateProviderProfile,
+  getSingleProvider,
+  getALLNearByProviders,
+  logoutProvider,
+} from "../Controllers/Provider.Controller.js";
 import { authMiddleware } from "../Middlewares/Auth.Middleware.js";
 
 const router = Router();
 
-router.route('/registerProvider').post(registerProvider);
-router.route('/updateProvider').put(authMiddleware, updateProviderProfile);
-router.route('/getProvider/:providerId').get(getSingleProvider);
-router.route('/getAllNearByProviders').get(authMiddleware, getALLNearByProviders);
-router.route('/logoutProvider').get(authMiddleware, logoutProvider);
+router.route("/registerProvider").post(upload.single("avatar"), registerProvider);
+router.route("/updateProvider").put(authMiddleware, updateProviderProfile);
+router.route("/getProvider/:providerId").get(getSingleProvider);
+router.route("/getAllNearByProviders").get(authMiddleware, getALLNearByProviders);
+router.route("/logoutProvider").get(authMiddleware, logoutProvider);
 
 export default router;
