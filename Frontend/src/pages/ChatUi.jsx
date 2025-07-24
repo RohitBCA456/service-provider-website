af6d15cdaffd71d98f03a85c08ae95b5c04e469e
RohitBCA456 <ry2281104@gmail.com>
2025-07-24 17:01:08 +0530

    fix

--- Frontend/src/pages/ChatUi.jsx
+++ Frontend/src/pages/ChatUi.jsx
@@ -56,6 +56,7 @@ const ChatPage = () => {
         );
         const data = await res.json();
         if (data.success) {
+          console.log(data)
           setCurrentUserId(data.user.id);
           console.log(data.user.id)
         } else {
